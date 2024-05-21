d1a3a040f28a84dacab3038050343daa5390f358
Nikunj Borad <nikunjborad2001@gmail.com>
2024-05-21 22:45:28 +0530

    pdf

--- components/intro.tsx
+++ components/intro.tsx
@@ -89,13 +89,21 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
-        <button
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          onClick={() => window.open('/Nikunj-resume-2024.pdf', '_blank')}
+        <Link
+          href={'/Nikunj-resume-2024.pdf'}
+          target="_blank"
+          rel="noopener noreferrer"
+          locale={false}
+          download
         >
-          Download CV{' '}
-          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </button>
+          <button
+            className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+            // onClick={() => window.open('/Nikunj-resume-2024.pdf', '_blank')}
+          >
+            Download CV{' '}
+            <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+          </button>
+        </Link>
       </motion.div>
     </section>
   );
