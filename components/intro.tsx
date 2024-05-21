62de5325807628ef7e7baab4b92fee4ce5c99196
Nikunj Borad <nikunjborad2001@gmail.com>
2024-05-21 22:11:26 +0530

    fix pdf download

--- components/intro.tsx
+++ components/intro.tsx
@@ -89,14 +89,13 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
 
-        <a
+        <button
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Nikunj-resume-2024.pdf"
-          download
+          onClick={() => window.open('/Nikunj-resume-2024.pdf', '_blank')}
         >
           Download CV{' '}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-        </a>
+        </button>
       </motion.div>
     </section>
   );
