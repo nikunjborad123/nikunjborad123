c3411c5847ff5165530049bf685bf65903ead7fb
Nikunj Borad <nikunjborad2001@gmail.com>
2024-05-21 22:34:24 +0530

    make output export

--- components/intro.tsx
+++ components/intro.tsx
@@ -91,7 +91,7 @@ export default function Intro() {
 
         <button
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          onClick={() => window.open('../public/Nikunj-resume-2024.pdf', '_blank')}
+          onClick={() => window.open('/Nikunj-resume-2024.pdf', '_blank')}
         >
           Download CV{' '}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
