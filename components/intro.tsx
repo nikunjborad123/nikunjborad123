91b1ff12b9c16687d3aa36bca65f02fb8c3a1fce
Nikunj Borad <nikunjborad2001@gmail.com>
2024-05-26 09:28:24 +0530

    added gsap animation

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,18 +1,61 @@
 'use client';
 
 import Image from 'next/image';
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useRef } from 'react';
 import Link from 'next/link';
 import { BsArrowRight } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
 import { useSectionInView } from '@/lib/hooks';
 import { useActiveSectionContext } from '@/context/active-section-context';
 import photo from '../public/nikunj.png';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import SplitType from 'split-type';
 
+gsap.registerPlugin(useGSAP);
 export default function Intro() {
+  const introRef = useRef<HTMLHeadingElement>(null);
+  const tl = useRef<GSAPTimeline>();
   const { ref } = useSectionInView('Home', 0.5);
   const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
+  useGSAP(
+    () => {
+      let text;
+      if (introRef.current) {
+        text = SplitType.create(introRef.current, { types: 'chars' });
+      }
+
+      tl.current = gsap
+        .timeline()
+        .from('.userAvatar', {
+          opacity: 1,
+          y: 200,
+          duration: 0.5,
+          ease: 'back.out(1.7)',
+        })
+        .from('.hand', {
+          opacity: 0,
+          y: 200,
+          duration: 0.3,
+          ease: 'back.out(1.7)',
+        })
+        .from(text?.chars as HTMLElement[], {
+          y: -50,
+          scaleY: 0,
+          opacity: 0,
+          stagger: 0.01,
+          duration: 0.5,
+          ease: 'circ.in',
+        })
+        .from('.buttonsGroup', {
+          opacity: 0,
+          duration: 0.3,
+          ease: 'power4.in',
+        });
+    },
+    { scope: '#home' }
+  );
 
   return (
     <section
@@ -22,14 +65,6 @@ export default function Intro() {
     >
       <div className="flex items-center justify-center">
         <div className="relative">
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: 'tween',
-              duration: 0.2,
-            }}
-          >
             <Image
               src={photo}
               alt="Nikunj Borad"
@@ -37,46 +72,25 @@ export default function Intro() {
               height="240"
               quality="95"
               priority={true}
-              className="h-28 w-28 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="userAvatar h-28 w-28 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
-          </motion.div>
 
-          <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: 'spring',
-              stiffness: 125,
-              delay: 0.1,
-              duration: 0.7,
-            }}
-          >
-            👋
-          </motion.span>
+          <span className="hand absolute bottom-0 right-0 text-4xl">👋</span>
         </div>
       </div>
 
-      <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
+      <h1
+        ref={introRef}
+        className="introText mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
       >
         <span>
           Hello, I'm <b>Nikunj</b>. I'm a <b>Frontend developer</b> with{' '}
           <b> 5 years </b> of experience. I enjoy building sites & apps. My
           focus is <b> React (Next.js)</b>.
         </span>
-      </motion.h1>
+      </h1>
 
-      <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium"
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.1,
-        }}
-      >
+      <div className="buttonsGroup flex flex-col sm:flex-row items-center justify-center gap-4 px-4 text-lg font-medium">
         <Link
           href="#contact"
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 hover:text-green-400 active:scale-105 transition"
@@ -98,13 +112,13 @@ export default function Intro() {
         >
           <button
             className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-            // onClick={() => window.open('/Nikunj-resume-2024.pdf', '_blank')}
+            onClick={() => window.open('/Nikunj-resume-2024.pdf', '_blank')}
           >
             Download CV{' '}
             <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
           </button>
         </Link>
-      </motion.div>
+      </div>
     </section>
   );
 }
